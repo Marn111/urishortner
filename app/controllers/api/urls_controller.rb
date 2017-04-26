3c91570277c7ba9d5c7abec18b35008ac1c2944c
Haymarn <haymarn@nexlabs.co>
2017-04-26 15:02:49 +0630

    modify urls#shortner api

--- app/controllers/api/urls_controller.rb
+++ app/controllers/api/urls_controller.rb
@@ -7,14 +7,12 @@ class Api::UrlsController < ActionController::Base
 		@url = Url.find_by(full_url: params[:full_url])
 		unless @url.present?
 			@url = Url.new(full_url: params[:full_url])
-			if @url.save
-				return render json: @url, status: 200
-			else
+			unless @url.save
 				return render json: @url.errors, status: 404
 			end
-		else
-			return render json: @url, status: 200
 		end
+		return render json: @url, status: 200
+		
 	end
 
 	def goto_full_address
