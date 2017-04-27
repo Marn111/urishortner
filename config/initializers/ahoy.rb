260c0ea4b33d6d555900f1e63c650161a008a3a2
Haymarn <haymarn@nexlabs.co>
2017-04-27 21:08:08 +0630

    call to super in track_visit of initializer/ahoy.rb

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,9 +1,7 @@
 class Ahoy::Store < Ahoy::Stores::ActiveRecordStore
   
   def track_visit(options)
-    super do |visit|
-      visit.gclid = visit_properties.landing_params["gclid"]
-    end
+    super
   end
   
 end
