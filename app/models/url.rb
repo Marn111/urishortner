cb9c4115bab603cfb5b0c58e70f67e797b8ad021
Haymarn <haymarn@nexlabs.co>
2017-04-27 20:59:25 +0630

    concat string

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,7 +6,7 @@ class Url < ActiveRecord::Base
 	def make_shortened_url
 		encoded_url = Digest::MD5.hexdigest(self.full_url + "with url shortner service")
 		self.full_url_hash = encoded_url
-		self.short_url = ENV['service_host'] + encoded_url.slice(0..6) 
+		self.short_url = "#{ENV['service_host']}" + encoded_url.slice(0..6) 
 	end
 
 end
